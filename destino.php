9e2bcf7ee72f411595f9f3d6a2879a0fc3f8f345
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-21 17:01:37 +0100

    Create destino.php

---
+++ destino.php
@@ -0,0 +1,8 @@
+<?php
+/* Se declaran con el GET: */
+$alfa=$_GET['alfa'];
+$bravo=$_GET['bravo'];
+/* Salen viajando por el navegador..: */
+echo "<p>variable \$alfa : $alfa";
+echo "<p>variable \$bravo : $bravo";
+?>
